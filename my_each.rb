b23a400872d6916365ee8965f7d0d38fd7c58886
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-22 12:35:07 -0400

    commit tree 45e799c5a0e9853ab7863491cb0af7815c4a0b3d

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  # interate over ebery element in an array
+  counter = 0
+  while counter < array.length #the counter is less than the length of the array
+  # yield to the block for each individual element
+    yield(array[counter])
+    counter += 1
+  end
+  array
 end
